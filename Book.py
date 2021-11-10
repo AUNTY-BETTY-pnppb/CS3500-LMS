7bb7aa5ef52ba6d10d8ee2ca4c4d7a9ae21a4a5e
Odhran Sexton <odhran.sexton@gmail.com>
2021-11-10 11:46:22 +0000

    Update __str__ method of Book
    You're welcome

--- Book.py
+++ Book.py
@@ -11,7 +11,7 @@ class Book:
         self._bookid = id(self)
 
     def __str__(self):
-        return '%s  |  %s  |  %s  |  %s' % (self._bookid, self._genre, self._name, self._author)
+        return 'Title: %s  | Author: %s  | Genre: %s  ' % (self._name, self._author, self._genre)
 
     def _getBookId(self):
         return self._bookid
